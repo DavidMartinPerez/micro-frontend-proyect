4b17906b09f430796bffcfe4063a7885f698f2b5
David Martín Pérez <davidmartinperez1@gmail.com>
2023-02-21 20:44:34 +0100

    [MFP] Solucionado bug

--- container/src/App.js
+++ container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <div style={{border: '1px solid blue'}}>
-            <h1>Container</h1>
+            <h1>Container!</h1>
             <hr/>
             <MarketingApp />
         </div>
