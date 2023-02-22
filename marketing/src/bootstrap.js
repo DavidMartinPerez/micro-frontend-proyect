e2d2173e5275b80d9b423cf3787a6c2ca6f3fbe8
David Martín Pérez <davidmartinperez1@gmail.com>
2023-02-22 08:14:43 +0100

    [MFP] Añadida multinavegación entre host(continer) y mf(marketing)

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
 
 
 // Función para renderizar la app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+
+    const history = defaultHistory || createMemoryHistory();
+
+    if(onNavigate) {
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />, el
+        <App history={history}/>, el
     )
+
+    return {
+        onParentNavigate({pathname: nextPathName}) {
+            const { pathname } = history.location;
+
+            if(pathname !== nextPathName) {
+                history.push(nextPathName);
+            }
+        }
+    }
 }
 
 // Si estamos en desarrollo o ejecutandolo aisladamente
@@ -15,7 +33,9 @@ if(process.env.NODE_ENV === 'development') {
     const root = document.querySelector('#dashboardApp');
 
     if(root) {
-        mount( root )
+        mount( root, {
+            defaultHistory: createBrowserHistory()
+        });
     }
 }
 
