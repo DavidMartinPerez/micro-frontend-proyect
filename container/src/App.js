6f617132f7463c1b712fbfc3e5b0ce17a2ce9735
David Martín Pérez <davidmartinperez1@gmail.com>
2023-02-22 07:29:32 +0100

    [MFP] Solucionado problema con el colisionado de estilos

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+import { StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'cnt'
+});
 
 export default () => {
     return (
-        <div style={{border: '1px solid blue'}}>
-            <h1>Container!</h1>
-            <hr/>
-            <MarketingApp />
+        <div style={{border: '3px solid blue'}}>
+            <BrowserRouter>
+                <StylesProvider generateClassName={generateClassName}>
+                    <div>
+                        <Header />
+                        <MarketingApp />
+                    </div>
+                </StylesProvider>
+            </BrowserRouter>
         </div>
     )
 }
